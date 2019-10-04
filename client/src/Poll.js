4747431e9ad456f2d854429d060ec497c46a59a4
zyhou <richard.maxime9@gmail.com>
2019-10-04 14:39:25 +0200

    Add poll design

--- client/src/Poll.js
+++ client/src/Poll.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './Poll.css';
 
 const Poll = () => {
+    const handleChange = () => {
+        console.log('change');
+    };
+
     return (
         <div className="container">
-            <button className="button-left">who</button>
-            <button className="button-right">who</button>
+            <label className="radio-left">
+                <input type="radio" name="answer" onChange={handleChange} />
+                <div>who</div>
+            </label>
+            <label className="radio-right">
+                <input type="radio" name="answer" onChange={handleChange} />
+                <div>who</div>
+            </label>
         </div>
     );
 };
