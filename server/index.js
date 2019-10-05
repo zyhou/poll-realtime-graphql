e8d25af5cd16d9e979de55ba2ab0a09848c3210b
Maxime <richard.maxime9@gmail.com>
2019-10-05 19:03:53 +0200

    Add Poll with Answers on percent by choice

--- server/index.js
+++ server/index.js
@@ -1,36 +1,68 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
-    type Book {
-        title: String
-        author: String
+    type Poll {
+        question: String!
+        anwsers: [Answer]
+    }
+
+    type Answer {
+        percent: Int
+        option: String
+    }
+
+    type Vote {
+        id: ID!
+        choice: Int!
     }
 
     type Query {
-        books: [Book]
+        poll: Poll
+    }
+
+    type Mutation {
+        addVote(id: ID!, choice: Int!): Vote!
     }
 `;
 
-const books = [
-    {
-        title: 'Harry Potter and the Chamber of Secrets',
-        author: 'J.K. Rowling',
-    },
-    {
-        title: 'Jurassic Park',
-        author: 'Michael Crichton',
-    },
-];
+const poll = {
+    question: 'On mange burger ?',
+    answers: [{ option: 'Oui' }, { option: 'Non' }],
+};
+
+const votes = [];
 
 const resolvers = {
     Query: {
-        books: () => books,
+        poll: () => ({ ...poll }),
+    },
+    Poll: {
+        anwsers: () => {
+            const sumChoice = votes.reduce((result, vote) => {
+                result[vote.choice] = (result[vote.choice] || 0) + 1;
+                return result;
+            }, {});
+
+            const nbVotes = votes.length;
+            const answers = poll.answers.map((answer, index) => ({
+                ...answer,
+                percent:
+                    Math.round((sumChoice[index + 1] / nbVotes) * 100) || 0,
+            }));
+
+            return answers;
+        },
+    },
+    Mutation: {
+        addVote: (_, { id, choice }) => {
+            votes.push({ id, choice });
+            return { id, choice };
+        },
     },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
